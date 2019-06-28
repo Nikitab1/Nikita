971982a78f2befaa53c61117b766430e1eb8f9d0
NIKITA <beelka18.0.18@icloid.com>
2019-06-28 15:21:07 +0300

    Сделал задания 1-5

---
+++ 1.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+#include <cstdlib> // ??? system
+using namespace std;
+
+int main()
+{
+    int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+    string bus;
+    getline(cin,bus);
+	for (int z = 0; z < bus.length(); z++) {
+		if (bus[z] =='a') {
+			a = a + 1;
+		}
+		if(bus[z] == 'e') {
+			e = e + 1;
+		}
+		if(bus[z] == 'i') {
+			i = i + 1;
+		}
+		if (bus[z] == 'o') {
+			o = o + 1;
+		}
+		if (bus[z] == 'u') {
+			u = u + 1;
+		}
+		if (bus[z] == 'y') {
+			y = y + 1;
+		}
+	}
+    cout << "a = " << a << endl;
+    cout << "e = " << e << endl;
+    cout << "o = " << o << endl;
+    cout << "u = " << u << endl;
+    cout << "i = " << i << endl;
+	
+    // system("pause"); // ?????? ??? ???, ? ???? MS Visual Studio
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> // ??? system
+using namespace std;
+void kolgl(string bus, int &a, int &e, int &o, int &u, int &i, int &y)
+{
+  for (int z = 0; z < bus.length(); z++) {
+		if (bus[z] == 'a') {		a = a + 1;		}	
+    	if (bus[z] == 'e') {		e = e + 1;		}	
+    	if (bus[z] == 'i') {	 	i = i + 1;	  	}	
+    	if (bus[z] == 'o') {		o = o + 1;		}	
+    	if (bus[z] == 'u') {		u = u + 1;		}	
+    	if (bus[z] == 'y') {		y = y + 1;		}
+	} 
+
+} 
+int main(){ 
+	int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+	 string bus; 
+	getline(cin,bus);
+	kolgl(bus,a,e,o,u,i,y);
+ 	// system("pause"); // ?????? ??? ???, ? ???? MS Visual Studio
+ 	cout << "a = " << a << endl; cout << "e = " << e << endl; cout << "o = " << o << endl; cout << "u = " << u << endl; cout << "i = " << i << endl;
+ 	return 0;
+}
